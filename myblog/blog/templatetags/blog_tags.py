f2490e6b4bfff3508a3a6d81bdd72a20c5b293e9
Kroll <113384296+KpoJleBapKa@users.noreply.github.com>
2025-11-28 09:16:45 +0200

    v2.5 creating a simple template tag

--- myblog/blog/templatetags/blog_tags.py
+++ myblog/blog/templatetags/blog_tags.py
@@ -1,9 +1,14 @@
 from django import template
 import markdown
 from django.utils.safestring import mark_safe
+from ..models import Post
 
 register = template.Library()
 
 @register.filter(name='markdown')
 def markdown_format(text):
     return mark_safe(markdown.markdown(text))
+
+@register.simple_tag
+def total_posts():
+    return Post.published.count()
